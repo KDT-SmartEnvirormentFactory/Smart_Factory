e8922dd7dd6dcc8cb56d311a06ab5ec111ac493a
Lawrence <chltmdqja01@gmail.com>
2026-02-13 15:38:17 +0900

    Create server_app.py for web server functionality
    Implement a Flask web server for the conveyor system.

---
+++ Conveyor_System/camera_env/server_app.py
@@ -0,0 +1,17 @@
+# 파일명: server_app.py
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    # templates 폴더 내부의 dashboard.html 파일을 찾아서 보여줍니다.
+    return render_template('dashboard.html')
+
+if __name__ == '__main__':
+    print("---------------------------------------------------")
+    print("🚀 관제 시스템 웹 서버가 시작되었습니다.")
+    print("🖥️  접속 주소: http://192.168.0.5:5000")
+    print("---------------------------------------------------")
+    # 0.0.0.0은 외부(같은 와이파이)에서 접속을 허용한다는 뜻입니다.
+    app.run(host='0.0.0.0', port=5001, debug=True)
